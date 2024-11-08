7df97ea7ab8ea0c7312b1edf7015a36f173dc694
Dev Protim Sikdar <dev.protimsikdar@brain-crowd.com>
2024-11-08 16:13:31 +0530

    removed bootstrap files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  // Your routes here
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
